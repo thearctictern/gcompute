a2e5106b888a7354573fb6835c24ed456de0af94
David Alexander <david@ternsoftware.org>
2018-07-28 19:49:26 +1000

    Initial commit

---
+++ tasks/instance.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# check that gcloud sdk has been installed
+
+if [[ -n $(find / -wholename */bin/gcloud 2> /dev/null) ]];then 
+  export $GCLOUD_PATH=$(find / -wholename */bin/gcloud 2> /dev/null | head -n 1)
+else
+  sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+    [google-cloud-sdk]
+    name=Google Cloud SDK
+    baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+    enabled=1
+    gpgcheck=1
+    repo_gpgcheck=1
+    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+  yum install google-cloud-sdk -y
+  export $GCLOUD_PATH=/usr/bin/gcloud
+fi
