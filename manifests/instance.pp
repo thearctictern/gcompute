d14d7d59733331f549d6e90d34f474e78705bdb8
David Alexander <davidmalexander83@hotmail.com>
2018-08-03 12:19:40 +1000

    add gcompute::instance

---
+++ manifests/instance.pp
@@ -0,0 +1,15 @@
+define gcompute::instance (
+  String $instance_name = $title,
+  String $project,
+  String $zone,
+  String $machinetype,
+  String $imagefamily,
+  String $imageproject,
+  String $sizeGB,
+  String $network,
+){
+  exec { "Idempotent GCP Create ${instance_name}":
+    command => "${gcloud_path} compute instances create ${instance_name} --project=${project} --zone=${zone} --machine-type=${machinetype} --create-disk=image-family=${imagefamily},image-project=${imageproject},size=${sizeGB} --image-family=${imagefamily} --image-project=${imageproject} --network=${network}",
+    unless  => "/bin/test \"\$(${gcloud_path} compute instances list | grep -c \'da-instance-1[[:space:]]\')\" -eq 1",
+  }
+}
