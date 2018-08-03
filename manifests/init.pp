24ce1a37a19ab3575d2bda8056c0b0b88532fc4e
David Alexander <davidmalexander83@hotmail.com>
2018-08-03 13:19:30 +1000

    fix things

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,12 @@
 # manage instance in GCP
 
 class gcompute(
-  String $gcloud_path,
   String $credential,
-  String $instance_name,
-  String $project,
-  String $zone,
-  String $machinetype,
-  String $imagefamily,
-  String $imageproject,
-  String $sizeGB,
-  String $network,
 ){
+  file { $credential:
+    ensure => file,
+    source => 'puppet:///modules/profile/gcp.json',
+  }->
   class { '::ruby':
     gems_version => 'latest',
   }->
@@ -28,9 +23,5 @@ class gcompute(
     scopes   => [
       'https://www.googleapis.com/auth/ndev.clouddns.readwrite',
     ],
-  }->
-  exec { 'Idempotent GCP Create':
-    command => "${gcloud_path} compute instances create ${instance_name} --project=${project} --zone=${zone} --machine-type=${machinetype} --create-disk=image-family=${imagefamily},image-project=${imageproject},size=${sizeGB} --image-family=${imagefamily} --image-project=${imageproject} --network=${network}",
-    unless  => "/bin/test \"\$(${gcloud_path} compute instances list | grep -c \'da-instance-1[[:space:]]\')\" -eq 1",
   }
 }
