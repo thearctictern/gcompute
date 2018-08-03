f92305c4c25e3e2afbfec58b68481e6ffcd8f3db
David Alexander <davidmalexander83@hotmail.com>
2018-08-03 13:23:08 +1000

    fix credential dep

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,5 +1,4 @@
 define gcompute::instance (
-  String $credential,
   String $gcloud_path,
   String $instance_name = $title,
   String $project,
@@ -10,13 +9,6 @@ define gcompute::instance (
   String $sizeGB,
   String $network,
 ){
-  gauth_credential { "mycred ${instance_name}":
-    path     => $credential,
-    provider => serviceaccount,
-    scopes   => [
-      'https://www.googleapis.com/auth/ndev.clouddns.readwrite',
-    ],
-  }
   exec { "Idempotent GCP Create ${instance_name}":
     command => "${gcloud_path} compute instances create ${instance_name} --project=${project} --zone=${zone} --machine-type=${machinetype} --create-disk=image-family=${imagefamily},image-project=${imageproject},size=${sizeGB} --image-family=${imagefamily} --image-project=${imageproject} --network=${network}",
     unless  => "/bin/test \"\$(${gcloud_path} compute instances list | grep -c \'${instance_name}[[:space:]]\')\" -eq 1",
