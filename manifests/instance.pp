cd9b984365a73afcba333006159e7a64b7b4e7b4
David Alexander <davidmalexander83@hotmail.com>
2018-08-03 12:28:11 +1000

    fix typo

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -11,6 +11,6 @@ define gcompute::instance (
 ){
   exec { "Idempotent GCP Create ${instance_name}":
     command => "${gcloud_path} compute instances create ${instance_name} --project=${project} --zone=${zone} --machine-type=${machinetype} --create-disk=image-family=${imagefamily},image-project=${imageproject},size=${sizeGB} --image-family=${imagefamily} --image-project=${imageproject} --network=${network}",
-    unless  => "/bin/test \"\$(${gcloud_path} compute instances list | grep -c \'da-instance-1[[:space:]]\')\" -eq 1",
+    unless  => "/bin/test \"\$(${gcloud_path} compute instances list | grep -c \'${instance_name}[[:space:]]\')\" -eq 1",
   }
 }
